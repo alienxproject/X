e493a5849e5f7b910c6f66c8ff302797b41e2ea5
alienxproject <142660399+alienxproject@users.noreply.github.com>
2023-09-24 21:27:50 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -149,7 +149,9 @@ fetch('https://raw.githubusercontent.com/alienxproject/X/main/assets/js/canales.
                 } else if (typeof yt_id !== 'undefined'){
                     containerTransmision.append(crearIframe(`https://www.youtube-nocookie.com/embed/live_stream?channel=${yt_id}&autoplay=1&mute=0&modestbranding=1&showinfo=0`));
                     barraNombre.append(crearBarraNombre(nombre, `https://www.youtube.com/channel/${yt_id}`));
-                } 
+                }  
+                // si posee enlace 'fuente' utlizalo en 'barraNombre'
+                if (typeof fuente !== 'undefined') {barraNombre.append(crearBarraNombre(nombre, fuente));}
                 // activa boton overlay y verifica su estado
                 btnCheckboxOverlay.disabled = false;
                 checkOverlayStatus();
